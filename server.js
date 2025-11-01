3c270013793e912b3aeb1e743760ff2bff380181
Shaik Akheel Ahmed <shaikakheelahmed1@gmail.com>
2025-11-01 12:05:53 +0530

    enable cors

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+server.use(cors());
+
 
 server.use(middlewares);
 server.use(router);
