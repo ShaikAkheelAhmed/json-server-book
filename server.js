d3049a76cd54364649fac0b6dc0cb2b0e92c1da7
Shaik Akheel Ahmed <shaikakheelahmed1@gmail.com>
2025-11-01 11:38:06 +0530

    changes in server and package.json

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-// server.js
-import jsonServer from "json-server";
+const jsonServer = require("json-server");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
