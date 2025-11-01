07536bda0b7adeff186017ba3e357e58876c6154
Shaik Akheel Ahmed <shaikakheelahmed1@gmail.com>
2025-11-01 12:13:55 +0530

    update cors

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+
+
 server.use(cors());
 
-
 server.use(middlewares);
+
 server.use(router);
 
 const port = process.env.PORT || 10000;
